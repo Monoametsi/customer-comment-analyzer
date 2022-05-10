112c2f23f0d4331df845a389f83db45aad3a09a8
Monoametsi Mosemeng <216026633@student.uj.ac.za>
2022-05-10 10:04:40 +0200

    Add server

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,43 @@
 import express, {Request,Response,Application} from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
+import * as path from 'path';
 import { MultipleFileReader } from './index';
+import {ReportResult} from './comments';
 
 const app:Application = express();
 
-const corsOptions = {
-    origin: 'http://localhost:4305',
-    credentials: true
+interface Cors {
+    origin: string
+}
+
+dotenv.config({path: path.join(__dirname, '.env')})
+
+const corsOptions: Cors = {
+    origin: 'http://localhost:4200'
 }
 
 app.use(cors(corsOptions));
+
+app.use(express.json());
 
 const PORT:number = Number(process.env.PORT) || 4000;
 
 app.get('/report-results', async (req:Request, res:Response) => {
 
     try{
-        const multipleFileReader = new MultipleFileReader(0);
+        const multipleFileReader: MultipleFileReader  = new MultipleFileReader(0);
         const reportResults = await multipleFileReader.printReportResults();
 
         return res.status(200).json({
             message: "Report results successfully delivered",
             reportResults
         });
-    }catch(err){
-
+    }catch(err: unknown){
+        res.status(503).json({
+            message: "Internal server error",
+            error:err
+        })
     }
 });
 
